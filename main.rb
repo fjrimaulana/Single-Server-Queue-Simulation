ff08ee5fb44b0868be9b76d3b54d88793444d264
Jonathan Bello <jonathanbello@icloud.com>
2014-09-07 20:34:43 -0400

    Output data in file

--- main.rb
+++ main.rb
@@ -1,14 +1,35 @@
 require 'spreadsheet'
 
 data = Spreadsheet.open 'algo1_2_1_AStimes.xls'
-
 sheet1 = data.worksheet 'Sheet1'
 
-sheet1.each do |arrival_time, service_time|
-  arrival_time = sheet1.column(1)
-  service_time = sheet1.column(2)
+data_arrived_time = []
+data_service_time = []
 
-  puts arrival_time
-  puts service_time
+sheet1.each do |row|
+  data_arrived_time.push(row[0])
+  data_service_time.push(row[1])
 end
 
+data_arrived_time.shift
+data_service_time.shift
+
+puts data_arrived_time
+puts data_service_time
+
+# jobs_to_do = data_arrived_time.count
+# puts jobs_to_do
+# i = 0
+#
+# arrival_time=0
+# delay_time=0
+#
+# service_begins = arrival_time + delay_time
+# service_time=0
+# wait_in_node = delay_time + service_time
+# departure_time = arrival_time + wait_in_node
+#
+# while i <= jobs_to_do
+#   i += 1
+#
+# end
